fdd89c494901165a6d361283b1a6c04d7b705676
Jihyun <hwgjh20@gmail.com>
2024-02-01 02:40:36 +0900

    first upload

---
+++ src/test/java/instagram/miniinstagram/MiniInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package instagram.miniinstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniInstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
